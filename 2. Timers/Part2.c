8d881c361d85f703a2c6424ff7467daa887d5139
Jacob Drennen <jakedren0526@gmail.com>
2023-02-16 17:43:15 -0500

    PART 2 done

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -10,6 +10,7 @@
 
 #include <msp430.h>
 
+unsigned int t = 50000;
 void gpioInit();
 void timerInit();
 
@@ -33,13 +34,23 @@ void gpioInit(){
     // @TODO Initialize the Red or Green LED
 
     // @TODO Initialize Button 2.3
-
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+    P6OUT &= ~BIT6;
+    P6DIR |= BIT6;
+    P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+    P2REN |= BIT3;                          // P2.3 pull-up register enable
+    P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+    P2IE |= BIT3;
 
 }
 
-void timerInit(){
+void timerInit()
+{
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
-
+    TB1CTL = TBSSEL_1 | MC_2| TBCLR;
+    TB1CCR0 = 50000;
+    TB1CCTL0 |= CCIE;
 }
 
 
@@ -51,6 +62,19 @@ void timerInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
+    P2IFG &= ~BIT3;
+    if(t == 50000)
+    {
+        t = 25000;
+    }
+    else if(t == 25000)
+    {
+        t = 10000;
+    }
+    else
+    {
+        t = 50000;
+    }
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
 
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
@@ -62,6 +86,9 @@ __interrupt void Port_2(void)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
+    P1OUT ^= BIT0;
+    TB1CCR0 += t;
+
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
 }
 
